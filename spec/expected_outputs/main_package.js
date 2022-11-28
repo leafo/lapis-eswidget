4a610f1b09cd6f91db6d7f04f5a581794cf955e0
leaf corcoran <leafot@gmail.com>
2022-11-27 18:00:24 -0800

    move expected outputs to separate files so we can track their changes via git diff

---
+++ spec/expected_outputs/main_package.js
@@ -0,0 +1,9 @@
+// spec/views/login.moon (main)
+window.init_Login = function(widget_selector, widget_params) {
+    console.log("Login!", widget_selector, widget_params)
+}
+
+// spec/views/user_profile.moon (main)
+window.init_UserProfile = function(widget_selector, widget_params) {
+    console.log("User profile!", widget_selector, widget_params)
+}
